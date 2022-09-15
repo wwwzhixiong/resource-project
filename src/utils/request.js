d88b8e80bba0df0fb4e7f3475c5a915ce98159fb
wwwzhixiong <2695122815@qq.com>
2022-09-15 16:45:59 +0800

    first-commit

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,4 +3,4 @@ const service = axios.create() // 创建了一个新的axios实例
 // 配置拦截器
 service.interceptors.request.use() // 请求拦截器主要处理 token的**`统一注入问题`**
 service.interceptors.response.use() // 响应拦截器 主要处理 返回的**`数据异常`** 和**`数据结构`**问题
-export default service // 导出，请求和响应拦截器，
+export default service // 暴漏 axios 实例
